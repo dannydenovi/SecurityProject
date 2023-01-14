f19c3a7273f3ecd765a7be32943da2c69dda3b3b
Claudio <claudio.anchesi@outlook.it>
2023-01-14 15:13:16 +0100

    Inizializzazione DB

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed -i "21iwww-data ALL=(ALL) NOPASSWD: ALL" /etc/sudoers;
+import -u www-data -p security_project < /var/www/db/db.sql;
 
 /usr/local/bin/docker-php-entrypoint "$@" && \
 	/usr/sbin/apache2ctl -D FOREGROUND
